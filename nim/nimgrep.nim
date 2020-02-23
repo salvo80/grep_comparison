b9a2620a2e6282ac675947947edbb104850f7c86
salvolupo <salvolupo.g@gmail.com>
2020-02-23 14:20:18 +0100

    added go

--- nim/nimgrep.nim
+++ nim/nimgrep.nim
@@ -1,7 +1,7 @@
 import os 
 import times, streams, strutils
 
-
+let buffSize=64000
 when isMainModule:
     let time = cpuTime()
     if not declared(paramCount) or paramCount() < 2:
@@ -9,13 +9,17 @@ when isMainModule:
     else:
         let word = paramStr(1)
         let filename = paramStr(2)
-        var n=1
-        #while strm.readLine(line) == true:
-        #    if line.contains(word):
-        #      echo n,": ",line
-        #    n = n+1
-        for line in readFile(filename).split('\n'):
-            if line.contains(word):
+        var s = newFileStream(filename, fmRead)
+        var line=""
+        var buff = newString(buffSize)
+        var n = 0
+        while not s.atEnd:
+            let block = s.readDataStr(buff, buff.len - n
+            if block.contains(word):
                 echo line
+            n = buff.len - n
+        #for line in readFile(filename).split('\n'):
+        #    if line.contains(word):
+        #        echo line
         #echo all.contains(word)
         echo cpuTime()-time
